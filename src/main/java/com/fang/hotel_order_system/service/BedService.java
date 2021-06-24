2473f1a2fc0f77e8cad33a4bba2c63cac63943eb
KaguraMea <1346785321@qq.com>
2021-06-24 12:18:51 +0800

    添加了Bed三类

--- src/main/java/com/fang/hotel_order_system/service/BedService.java
+++ src/main/java/com/fang/hotel_order_system/service/BedService.java
@@ -2,6 +2,9 @@ package com.fang.hotel_order_system.service;
 
 import com.fang.hotel_order_system.entity.Bed;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.fang.hotel_order_system.entity.Breakfast;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,25 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface BedService extends IService<Bed> {
 
+    int deleteByPrimaryKey(Integer bedId);
+
+    int insert(Bed record);
+
+    int insertSelective(Bed record);
+
+    Breakfast selectByPrimaryKey(Integer bedId);
+
+    List<Breakfast> selectRoles(Breakfast record);
+
+    List<Breakfast> selectRoles(Bed record);
+
+    int updateByPrimaryKeySelective(Breakfast record);
+
+    int updateByPrimaryKey(Breakfast record);
+
+    int updateByPrimaryKeySelective(Bed record);
+
+    int updateByPrimaryKey(Bed record);
+
+    void assign(Integer breakfastId, Integer[] checkMenuIds);
 }
--- src/main/java/com/fang/hotel_order_system/service/impl/BedServiceImpl.java
+++ src/main/java/com/fang/hotel_order_system/service/impl/BedServiceImpl.java
@@ -1,10 +1,15 @@
 package com.fang.hotel_order_system.service.impl;
 
 import com.fang.hotel_order_system.entity.Bed;
+import com.fang.hotel_order_system.entity.Breakfast;
 import com.fang.hotel_order_system.mapper.BedMapper;
+import com.fang.hotel_order_system.mapper.BreakfastMapper;
 import com.fang.hotel_order_system.service.BedService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -15,6 +20,74 @@ import org.springframework.stereotype.Service;
  * @since 2021-06-14
  */
 @Service
-public class BedServiceImpl extends ServiceImpl<BedMapper, Bed> implements BedService {
+public abstract class BedServiceImpl extends ServiceImpl<BedMapper, Bed> implements BedService {
+    @Resource
+    private BedMapper mapper;
 
+    @Override
+    public int deleteByPrimaryKey(Integer bedId) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int insert(Bed record) {
+        return mapper.insert(record);
+    }
+
+    @Override
+    public int insertSelective(Bed record) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Breakfast selectByPrimaryKey(Integer bedId) {
+        return mapper.selectByPrimaryKey(bedId);
+    }
+
+    @Override
+    public List<Breakfast> selectRoles(Bed record) {
+        return mapper.queryList(record);
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(Bed record) {
+        return 0;
+    }
+
+    @Override
+    public int updateByPrimaryKey(Bed record) {
+        return mapper.updateByPrimaryKey(record);
+    }
+
+    @Override
+    public void assign(Integer bedId, Integer[] checkMenuIds) {
+        // 先删除原来已经分配的角色和菜单的数据
+        mapper.deleteAllMenuOfBed(bedId);
+        // 在重新插入新选择的菜单项
+        if (checkMenuIds != null && checkMenuIds.length >= 1) {
+            for (Integer menuId : checkMenuIds) {
+                // 插入到角色和菜单中间表
+                mapper.saveBedMenu(bedId, menuId);
+            }
+        }
+    }
+    public Integer getUserCountUseBreakfast(Integer bedId){
+        mapper.getUserCountUseBed(bedId);
+        return bedId;
+    }
+
+    /**
+     * 删除角色和角色相关信息
+     */
+    public void deleteBedInfo (Integer bedId){
+// 删除用户和角色中间表信息
+        mapper.deleteUserBed(bedId);
+// 删除角色和菜单中间表信息
+        mapper.deleteBedMenu(bedId);
+
+// 删除角色信息
+        mapper.deleteByPrimaryKey(bedId);
+    }
 }
