1d16533c75cb6c88cbcb9aa631cd67c157405d39
DarkFFang <45850696+DarkFFang@users.noreply.github.com>
2021-06-27 12:45:16 +0800

    null

--- src/main/java/com/fang/hotel_order_system/mapper/BedMapper.java
+++ src/main/java/com/fang/hotel_order_system/mapper/BedMapper.java
@@ -2,7 +2,10 @@ package com.fang.hotel_order_system.mapper;
 
 import com.fang.hotel_order_system.entity.Bed;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fang.hotel_order_system.entity.Breakfast;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,4 +18,21 @@ import org.springframework.stereotype.Component;
 @Component
 public interface BedMapper extends BaseMapper<Bed> {
 
+    Breakfast selectByPrimaryKey(Integer bedId);
+
+    List<Breakfast> queryList(Bed record);
+
+    int updateByPrimaryKey(Bed record);
+
+    void deleteAllMenuOfBed(Integer bedId);
+
+    void saveBedMenu(Integer bedId, Integer menuId);
+
+    void getUserCountUseBed(Integer bedId);
+
+    void deleteUserBed(Integer bedId);
+
+    void deleteBedMenu(Integer bedId);
+
+    void deleteByPrimaryKey(Integer bedId);
 }
